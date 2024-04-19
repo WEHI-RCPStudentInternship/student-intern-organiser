efdbf30d926402d6f0239b4f4a0a97771b16cb6f
XiaoHan-Guo <guoxiaohang@MacBook-Pro-5.local>
2024-04-19 12:21:56 +1000

    1

--- app.py
+++ app.py
@@ -89,7 +89,81 @@ def download_empty_emails():
         mimetype="text/csv",
         headers={"Content-disposition":
                  "attachment; filename=empty_email_users.csv"})
+def update_students_by_criteria(criteria, update_fields):
+    # Check if criteria and update_fields are not empty
+    if not criteria or not update_fields:
+        return "Criteria and update fields cannot be empty."
 
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+
+        # Constructing the WHERE part of the SQL query from criteria
+        criteria_query_parts = [f"{key} = ?" for key in criteria.keys()]
+        criteria_query = " AND ".join(criteria_query_parts)
+
+        # Constructing the SET part of the SQL query from update_fields
+        update_query_parts = [f"{key} = ?" for key in update_fields.keys()]
+        update_query = ", ".join(update_query_parts)
+
+        # Constructing the complete SQL query
+        sql_query = f"UPDATE Students SET {update_query} WHERE {criteria_query}"
+
+        # Executing the query
+        cursor.execute(sql_query, list(update_fields.values()) + list(criteria.values()))
+        conn.commit()
+
+        if cursor.rowcount == 0:
+            print("No rows were updated. Please check your criteria.")
+            return
+        else:
+            print(f"Successfully updated {cursor.rowcount} row(s).")
+            return
+
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+        return
+    finally:
+        if conn:
+            conn.close()
+
+
+def update_email(csv_path):
+    with open(csv_path, mode='r', encoding='utf-8') as file:
+        csv_reader = csv.reader(file)
+        # Skip the header
+        next(csv_reader, None)
+        # Read the data into a list of tuples
+        data_list = [(row[0], row[1], row[2], row[3]) for row in csv_reader]
+    
+    print(data_list)
+    
+    for student_id, student_name, student_email, wehi_email in data_list:
+        criteria = {"intern_id": student_id, "email": student_email, "full_name":student_name}
+        update_fields = {"wehi_email": wehi_email}
+        update_students_by_criteria(criteria, update_fields)
+
+
+
+
+
+
+
+@app.route('/update_wehi',methods=['GET', 'POST'])
+def update_wehi():
+    if request.method == 'POST':
+        if 'csv_file' in request.files:
+            file = request.files['csv_file']
+            if file and file.filename != '':
+                filename = file.filename
+                filepath = 'student_intern_data/attachments/' + filename
+                file.save(filepath)
+                update_email(filepath)
+            else:
+                pass
+        else:
+            pass
+    return render_template('2024Update_Wehi.html')
 
 
 

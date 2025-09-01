9ffacf3efc6afc574aa7513f6e7be8a53fbc9661
102619835 <115048372+102619835@users.noreply.github.com>
2025-09-01 22:11:43 +1000

    Fix: intake_email.sql fix position

--- sql_upgrades/intake_email.sql
+++ sql_upgrades/intake_email.sql
@@ -1,4 +1,3 @@
-
 -- Create Email Schedule table for storing intake email dates and content
 CREATE TABLE IF NOT EXISTS EmailSchedule (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -20,12 +19,10 @@ CREATE TABLE IF NOT EXISTS EmailSchedule (
 -- Week 4 (Fourth week): 21 days (Monday of week 4)
 -- Week 10 (Tenth week): 63 days (Monday of week 10 = 7 x 9 days after start)
 -- Week 13 (End): 84 days (Monday of week 13 = 7 x 12 days after start)
-INSERT INTO EmailSchedule (id, intake_id, week_number, week_offset_days, subject, body) VALUES
-(1, 1, '0 - 1 week before', -7, '1 week before WEHI internship', 'Hi All, This is your email to start your internship preparation...'),
-(2, 1, '1 - First week', 0, 'First week of WEHI internship', 'Hi All, Welcome to your first week of internship...'),
-(3, 1, '2 - Second week', 7, 'Second week of WEHI internship', 'Hi All, This is your second week of internship...'),
-(4, 1, '4 - Fourth week', 21, 'Fourth week of WEHI internship', 'Hi All, This is your email to start week 4 of your internship...'),
-(5, 1, '10 - Tenth week', 63, 'Tenth week of WEHI internship', 'Hi All, This is your email to start week 10 of your internship...'),
-(6, 1, '13 - End of internship', 84, 'End of WEHI internship', 'Hi All, I would like to thank you all for being a part of this intake...');
-
--- (Other .sql files were empty and are not included)
+INSERT INTO EmailSchedule (intake_id, week_number, week_offset_days, subject, body) VALUES
+(1, '0 - 1 week before', -7, '1 week before WEHI internship', 'Hi All, This is your email to start your internship preparation...'),
+(1, '1 - First week', 0, 'First week of WEHI internship', 'Hi All, Welcome to your first week of internship...'),
+(1, '2 - Second week', 7, 'Second week of WEHI internship', 'Hi All, This is your second week of internship...'),
+(1, '4 - Fourth week', 21, 'Fourth week of WEHI internship', 'Hi All, This is your email to start week 4 of your internship...'),
+(1, '10 - Tenth week', 63, 'Tenth week of WEHI internship', 'Hi All, This is your email to start week 10 of your internship...'),
+(1, '13 - End of internship', 84, 'End of WEHI internship', 'Hi All, I would like to thank you all for being a part of this intake...');
